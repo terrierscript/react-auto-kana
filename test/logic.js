c0faf1bd000ab651b457b7cb52c0fa0814c84ab9
suisho <suisho000000@gmail.com>
2015-03-08 14:57:04 +0900

    invert if

--- test/logic.js
+++ test/logic.js
@@ -7,12 +7,48 @@ describe("logic", function(){
       value: "山田",
       prev : "やまだ"
     })
-    var expect = {
+    assert.deepEqual("やまだ", next.kana)
+  })
+  it("add", function(){
+    var next = logic({
+      value: "山田た",
       kana : "やまだ",
       prev : "山田",
-      dict : [["山田", "やまだ"]]
-    }
-    assert.deepEqual(expect.dict, next.dict)
-    assert.deepEqual(expect, next)
+      dict : [
+        ["山田" , "やまだ"]
+      ]
+    })
+    assert.deepEqual("やまだた", next.kana)
+  })
+  it("山田たろう -> 山田太郎", function(){
+    var next = logic({
+      prev : "山田たろう",
+      value: "山田太郎",
+      kana : "やまだたろう",
+      dict : [
+        ["山田" , "やまだ"]
+      ]
+    })
+    assert.deepEqual("やまだたろう", next.kana)
+  })
+  it("まりお -> 鞠男 -> 毬男", function(){
+    var steps = ["まりお" ,"鞠男", "毬男"]
+    steps.reduce(function(state, value){
+      state.value = value
+      var result = logic(state)
+      assert.equal("まりお", result.kana)
+      return result
+    }, {})
+  })
+  it("remove", function(){
+    var next = logic({
+      value: "山",
+      kana : "やまだ",
+      prev : "山田",
+      dict : [
+        ["山田" , "やまだ"]
+      ]
+    })
+    assert.deepEqual("やまだ", next.kana) // not change
   })
 })
