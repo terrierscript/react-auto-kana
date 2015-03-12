275b652c813cf76eefb60a3319f5bf8b485a5466
suisho <suisho000000@gmail.com>
2015-03-13 01:09:36 +0900

    move mode detector

--- src/kana.js
+++ src/kana.js
@@ -1,13 +1,40 @@
 var extend = require("extend")
+var diff = require("compact-diff")
 var slotLogic = require("./slot")
+
 var buildKana = function(slots){
   return slots.map(function(slot){
     return slot.kana
   }).join("")
 }
 
+var getMode = function(prev, current){
+  var diffPack = diff(prev, current)
+  for(var i = 0; i < diffPack.length; i++){
+    var d = diffPack[i]
+    if(d.added && d.removed){
+      return "converted"
+    }
+    if(d.added){
+      return "added"
+    }
+    if(d.removed){
+      return "removed"
+    }
+  }
+  return "none"
+}
+
+var getConvertPair = function(prev, current){
+  var diffPack = diff(prev, current)
+  return diffPack.filter(function(d){
+    return (d.added || d.removed)
+  })
+}
+
 module.exports = function(state){
   var slots = state.slots || []
+  // polyfill slots
   var slot = extend({}, slots[0], {
     value : state.value
   })
--- src/tmp.js
+++ src/tmp.js
@@ -1,26 +1,5 @@
 // wip
-var getMode = function(prev, current){
-  var diffPack = diff(prev, current)
-  for(var i = 0; i < diffPack.length; i++){
-    var d = diffPack[i]
-    if(d.added && d.removed){
-      return "converted"
-    }
-    if(d.added){
-      return "added"
-    }
-    if(d.removed){
-      return "removed"
-    }
-  }
-}
 
-var getConvertPair = function(prev, current){
-  var diffPack = diff(prev, current)
-  return diffPack.filter(function(d){
-    return (d.added || d.removed)
-  })
-}
 
 var build = function(state){
   var prev = japanese.hiraganize(state.prev || "")
