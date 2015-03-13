bdb12814096525bc815bbf068b860c3b9ac6b2d1
suisho <suisho000000@gmail.com>
2015-03-13 21:01:20 +0900

    refactor partial

--- src/partial.js
+++ src/partial.js
@@ -8,33 +8,51 @@ var convert = function(arr){
   })
 }
 
-module.exports = function(splits, value){
-  var rest = value
-  var spl = convert(splits)
-  var i, part, reg // TODO: let?
-  // 前方
-  for( i = 0; i < spl.length; i++){
-    part = spl[i]
-    reg = new RegExp("^" + part.value)
+var preFilter = function(spl, rest){
+  for(var i = 0; i < spl.length; i++){
+    var part = spl[i]
+    var reg = new RegExp("^" + part.value)
     if(!reg.test(rest)){
       break
     }
     spl[i].matched = true
     rest = rest.replace(reg, "")
   }
-  //console.log(spl, value)
-  for( i = 0; i < spl.length; i++){
-    var j = splits.length - i - 1
-    part = spl[j]
-    reg = new RegExp(part.value + "$")
+  return {
+    slots : spl,
+    rest : rest
+  }
+}
+
+var postFilter = function(spl, rest){
+  for(var i = 0; i < spl.length; i++){
+    var last = spl.length - i - 1
+    var part = spl[last]
+    var reg = new RegExp(part.value + "$")
     if(!reg.test(rest)){
       break
     }
-    spl[j].matched = true
+    spl[last].matched = true
     rest = rest.replace(reg, "")
   }
+  return {
+    slots : spl,
+    rest : rest
+  }
+}
+
+module.exports.add = function(splits, value){
+}
+
+module.exports.convert = function(splits, value){
+  var slots = convert(splits)
+  // 前方
+  var preFilterd = preFilter(slots, value)
+  var postFilterd = postFilter(preFilterd.slots, preFilterd.rest)
+  //console.log(spl, value)
   //console.log(spl, value, rest)
-  return spl.map(function(parts){
+  var rest = postFilterd.rest
+  return postFilterd.slots.map(function(parts){
     if(parts.matched){
       return {
         value : parts.value
--- test/partial.js
+++ test/partial.js
@@ -2,7 +2,7 @@ var partial = require("../src/partial")
 var assert = require("power-assert")
 describe("partial", function(){
   it("simple", function(){
-    var result = partial(["山田", "たろう"], "山田太郎")
+    var result = partial.convert(["山田", "たろう"], "山田太郎")
     var expect = [
       { value : "山田" },
       { value : "たろう", changed : "太郎"}
@@ -10,7 +10,7 @@ describe("partial", function(){
     assert.deepEqual(result, expect)
   })
   it("simple", function(){
-    var result = partial(["山田", "たろう"], "山たろう")
+    var result = partial.convert(["山田", "たろう"], "山たろう")
     var expect = [
       { value : "山田" , changed : "山" },
       { value : "たろう"}
@@ -18,14 +18,14 @@ describe("partial", function(){
     assert.deepEqual(result, expect)
   })
   it.skip("double change", function(){
-    var result = partial(["山田", "太郎"], "山本次郎")
+    var result = partial.convert(["山田", "太郎"], "山本次郎")
     var expect = [
       { value : "山田" , changed : "山本" },
       { value : "太郎", changed : "次郎"}
     ]
   })
   it.skip("double change", function(){
-    var result = partial(["山田", "たろう"], "山ろう")
+    var result = partial.convert(["山田", "たろう"], "山ろう")
     var expect = [
       { value : "山田" , changed : "山" },
       { value : "たろう", changed : "ろう"}
