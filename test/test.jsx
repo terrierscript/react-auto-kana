17343dda28d4aa31fa87711831bb725cbc47aadb
suisho <suisho000000@gmail.com>
2015-03-29 13:59:12 +0900

    remake test

--- test/test.jsx
+++ test/test.jsx
@@ -2,22 +2,23 @@ var assert = require("power-assert")
 var React = require("react/addons")
 var jsdom = require('mocha-jsdom');
 var AutoKana = require("../src/AutoKana.jsx")
-
+var TestUtils = React.addons.TestUtils
 describe("Component", function(){
   jsdom();
-  it("do", function(done){
-    var updateFunc = function(state){
-      console.log(state)
-      // done()
+  it("do", function(){
+    function noop(){ // noop
     }
-    var component = React.addons.TestUtils.renderIntoDocument(<AutoKana  onUpdate={updateFunc}/>)
-    var node = React.findDOMNode(component.refs.input);
-    React.addons.TestUtils.Simulate.change(node,
-      { target: { value: 'やまだ'}
-    });
-    React.addons.TestUtils.Simulate.change(node,
-      { target: { value: '山田'}
-    });
+
+    var component = TestUtils.renderIntoDocument(<AutoKana onUpdate={noop}/>)
+    var node = TestUtils.findRenderedDOMComponentWithTag(component, "input");
+
+    var inputHistory = ["ｙ","や","やｍ","やま","やまｄ","やまだ","山田","山田","山田ｔ","山田た","山田たｒ","山田たろ","山田たろう","山田太郎","山田太郎"]
+    inputHistory.forEach(function(v){
+      TestUtils.Simulate.change(node,
+        { target: { value: v}
+      })
+    })
+    assert.equal(component.state.kana, "やまだたろう")
 
   })
 })
